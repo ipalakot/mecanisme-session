3913681c5213593b9e79d950c0cad106b81699f3
ipalakot <ipalakot@gmail.com>
2019-10-02 15:53:37 +0200

    Ctrlr _cardçadd()

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class CartController extends AbstractController
 {
@@ -13,5 +19,22 @@ class CartController extends AbstractController
     public function index()
     {
         return $this->render('cart/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/panier/add/{id}", name="card_add")
+     * 
+     */
+    public function add($id, SessionInterface $session)
+    {
+        $panier = $session->get('panier', []);
+
+        if( !empty($panier[$id])) {
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+        }
+        $session->set('panier', $panier);
+        dd($session->get('panier'));
     }
 }
